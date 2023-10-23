f91fa77945311b20d9cec379466479fec7a3d7e5
Mike Vardy <mike.vardy@redspace.com>
2023-10-23 05:29:48 -0400

    init

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    register: true
+})
+
+module.exports = withPWA({
+    // next.js config
+})
